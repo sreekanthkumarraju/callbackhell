cf99fcda10ee70c5eb975339c5b72c495cc34273
sreekanthkumarraju <rvsreekanthkumar90@gmail.com>
2022-04-15 15:56:19 +0530

    callback

---
+++ countdown.js
@@ -0,0 +1,39 @@
+let count=document.querySelector('.countdown')
+
+let time=10
+
+
+setTimeout(()=>{
+    count.innerText=time--
+     setTimeout(()=>{
+         count.innerText=time--
+           setTimeout(()=>{
+               count.innerText=time--
+               setTimeout(()=>{
+                   count.innerText=time--
+                   setTimeout(()=>{
+                       count.innerText=time--
+                       setTimeout(()=>{
+                           count.innerText=time--
+                           setTimeout(()=>{
+                               count.innerText=time--
+                              setTimeout(()=>{
+                                  count.innerText=time--
+                                  setTimeout(()=>{
+                                      count.innerText=time--
+                                      setTimeout(()=>{
+                                          count.innerText=time--
+                                          setTimeout(()=>{
+                                              count.innerText="Happy Independence Day"
+                                          },1000)
+                                      },1000)
+                                      
+                                  },1000)
+                              },1000)
+                           },1000)
+                       },1000)
+                   },1000)
+               },1000)
+           },1000)
+     },1000)
+},1000)
